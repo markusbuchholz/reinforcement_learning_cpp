e2a3251beb22903614846fa8a4fbbe4667e09e9f
markusbuchholz <markus.x.buchholz@gmail.com>
2020-08-23 22:02:41 +0200

    all implmented

--- 1d_q_learn.cpp
+++ 1d_q_learn.cpp
@@ -1,46 +1,70 @@
 #include <iostream>
 #include <map>
 #include <vector>
-#include <thread> //wait
-#include<chrono> //wait
-#include<stdlib.h> //rsrand, rand
-#include<time.h> //random
+#include <thread>   //wait
+#include <chrono>   //wait
+#include <stdlib.h> //rsrand, rand
+#include <time.h>   //random
 
+//========================================================================================
 
-int N_STATES = 6;         //number of states
-float EPSILON = 0.8;      // greedy policy
-float ALPHA = 0.1;        //learning rate
-float GAMMA = 0.9;        //discount factor
-int MAX_EPISODES = 15;    //number of episodes the agent is going to play
+int N_STATES = 6;       //number of states
+float EPSILON = 0.8;    // greedy policy
+float ALPHA = 0.1;      //learning rate
+float GAMMA = 0.9;      //discount factor
+int MAX_EPISODES = 15;  //number of episodes the agent is going to play
 int REFRESH_RATE = 300; //time for one move
 
 std::string a_left = "left";
 std::string a_right = "right";
 std::vector<std::string> ACTIONS{"left", "right"};
+//std::string rl_action;
 
-std::vector<std::pair<std::string, double>> q_table;
-std::vector<std::pair<std::string, double>> q_table_main;
+std::vector<std::pair<double, double>> q_table;
+std::vector<std::pair<double, double>> q_table_main;
 
+//========================================================================================
 
+auto Choose_Action(int state, std::vector<std::pair<double, double>> q_table)
+{
 
+    std::string action{};
+    double value_state_1 = q_table[state].first;
+    double value_state_2 = q_table[state].second;
 
-auto Choose_Action(int state, std::vector<std::pair<std::string, double>> q_table){
-    float rand_value = rand()%10/10.0;
-    std::cout<<":: "<< rand_value << std::endl;
+    double rand_value = ((double)rand() / (RAND_MAX));
 
-    double value_state_1 = q_table[state].second;
-    double value_state_2 = q_table[state+1].second;
+    std::cout << ":: " << rand_value << std::endl;
 
-
-    if ((rand_value>EPSILON) || ((value_state_1 == 0) && (value_state_2 == 0)))
+    if ((rand_value > EPSILON) || ((value_state_1 == 0) && (value_state_2 == 0)))
     {
-        std::cout<<"value 0"<< std::endl;
+        std::cout << "choosing random, Exploration ... " << std::endl;
+        if ((double)rand() / RAND_MAX >= 0.5)
+        {
+            action = "left";
+        }
+        else
+        {
+            action = "right";
+        }
     }
-    else {
-        std::cout<<" higher value" << std::endl;
+    else
+    {
+        std::cout << " follow the Explotation policy..." << std::endl;
+        if (value_state_1 >= value_state_2)
+        {
+            action = q_table[state].first;
+        }
+        else
+        {
+            action = q_table[state + 1].first;
+        }
     }
+
+    return action;
 }
 
+//========================================================================================
 
 void Update_Env(int state, int episode, int step_counter)
 {
@@ -64,46 +88,165 @@ void Update_Env(int state, int episode, int step_counter)
 
     std::this_thread::sleep_for(std::chrono::milliseconds(REFRESH_RATE));
 }
+//========================================================================================
 
 //std::vector<std::pair<std::string, double>> Build_Q_Table(int n_states) //, std::vector<std::string> actions)
 auto Build_Q_Table(int n_states)
 {
-
-    std::vector<std::pair<std::string, double>> q_tab;
+    //std::vector<std::vector<std::pair<std::string, double>>> q_tab;
+    std::vector<std::pair<double, double>> q_tab;
 
     for (int i = 0; i < n_states; i++)
     {
         //temp_map.insert(i, std::make_pair)
-        q_tab.push_back(std::make_pair("left", 0.0));
-        q_tab.push_back(std::make_pair("right", 0.0));
+        //temp_vector.push_back(std::make_pair("left", 0.0));
+        q_tab.push_back(std::make_pair(0.0, 0.0));
+        //q_tab.push_back(temp_vector);
+    }
+
+    int state{0};
+    for (auto &i : q_tab)
+    {
+
+        std::cout << " state :: " << state << " left :: " << i.first << " right :: " << i.second << std::endl;
+        state++;
     }
 
     return q_tab;
 }
 
+//========================================================================================
+
+auto Get_Env_Feedback(int state, std::string action)
+{
+
+    std::string terminal{};
+    int next_state{};
+    int reward{};
+    std::vector<int> return_vector{};
+
+    if (action == "right")
+    {
+        if (state = N_STATES - 2)
+        {
+            next_state = -999; //terminal
+            reward = 1;
+        }
+        else
+        {
+            next_state = state + 1;
+            reward = 0;
+        }
+    }
+    else
+    {
+        reward = 0;
+        if (state == 0)
+        {
+            next_state = state;
+        }
+        else
+        {
+            next_state = state - 1;
+        }
+    }
+
+    return_vector.push_back(next_state);
+    return_vector.push_back(reward);
+
+    return return_vector;
+}
+
+//========================================================================================
 //std::vector<std::pair<std::string, double>>
 auto Run_Rl()
 {
-
-   q_table = Build_Q_Table(N_STATES);//, ACTIONS);
+    int state{};
+    int step_counter{};
     int ijk = 0;
+    bool is_terminated{};
+    std::string action{};
+    std::vector<int> return_vector{};
+    int next_state{};
+    int reward{};
+    double q_target{};
+    double q_value_next_state_action{};
+
+    q_table = Build_Q_Table(N_STATES); //, ACTIONS);
+
     for (auto &i : q_table)
     {
 
         std::cout << "state :: " << ijk << ":: action :: " << i.first << ":: value :: " << i.second << std::endl;
         ijk++;
     }
+
+    for (int episode = 0; episode < MAX_EPISODES; episode++)
+    {
+
+        std::cout << "EPISODE NR ::: " << episode << std::endl;
+        step_counter = 0;
+        state = 0;
+        is_terminated = false;
+        Update_Env(state, episode, step_counter);
+
+        while (!is_terminated)
+        {
+
+            action = Choose_Action(state, q_table);
+            return_vector = Get_Env_Feedback(state, action);
+            next_state = return_vector[0];
+            reward = return_vector[1];
+
+            if (q_table[next_state].first >= q_table[next_state].second)
+            {
+                q_value_next_state_action = q_table[next_state].first;
+            }
+            else
+            {
+                q_value_next_state_action = q_table[next_state].second;
+            }
+
+            if (next_state == -999) //terminal
+            {
+
+                q_target = reward + GAMMA * q_value_next_state_action;
+            }
+            else
+            {
+                q_target = reward;
+                is_terminated = true;
+            }
+
+            if (action == "left")
+            {
+
+                q_table[state].first = q_table[state].first + ALPHA * (q_target - q_value_next_state_action);
+            }
+
+            if (action == "right")
+            {
+
+                q_table[state].second = q_table[state].second + ALPHA * (q_target - q_value_next_state_action);
+            }
+
+            state = next_state;
+            Update_Env(state, episode, step_counter + 1);
+        }
+    }
+
     return q_table;
 }
 
+//=======================
+//      MAIN
+//=======================
 
 int main()
 {
 
     srand(time(NULL));
     q_table_main = Run_Rl();
-    Update_Env(0, 10, 10);
-    Choose_Action(2, q_table);
-
+    //Build_Q_Table(6);
     return 0;
 }
